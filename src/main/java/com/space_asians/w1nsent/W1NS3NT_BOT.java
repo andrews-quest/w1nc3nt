abb2dd61b7cf511c5f6c98f3480a71a9c22255c7
andrews-quest <true.andrewsquest@gmail.com>
2024-04-17 17:07:11 +0200

    The simplest echo bot, based on TelegramBots library.

---
+++ src/main/java/com/space_asians/w1nsent/W1NS3NT_BOT.java
@@ -0,0 +1,42 @@
+package com.space_asians.w1nsent;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.message.Message;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+import java.util.List;
+
+
+public class W1NS3NT_BOT implements LongPollingSingleThreadUpdateConsumer {
+
+    @Value("${telegram.bot.username}")
+    private String username;
+    // @Value("${telegram.bot.test_token}")
+    // private String token;
+
+    private final TelegramClient telegramClient;
+    public W1NS3NT_BOT(String token){
+       telegramClient = new OkHttpTelegramClient(token);
+    }
+
+    @Override
+    public void consume(Update update) {
+        System.out.println("consumed");
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            Message message = update.getMessage();
+            String text = message.getText();
+            SendMessage sm = SendMessage.builder().chatId(message.getChatId()).text(text).build();
+            // sm.setText(text);
+            // sm.setChatId(String.valueOf(message.getChatId()));
+            try{
+                telegramClient.execute(sm);
+            } catch (TelegramApiException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
--- src/main/java/com/space_asians/w1nsent/W1nsentApplication.java
+++ src/main/java/com/space_asians/w1nsent/W1nsentApplication.java
@@ -1,13 +1,25 @@
 package com.space_asians.w1nsent;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 
-@SpringBootApplication
+
+
 public class W1nsentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(W1nsentApplication.class, args);
+		// SpringApplication.run(W1nsentApplication.class, args);
+
+
+
+		String token = "";
+		try(TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+			botsApplication.registerBot(token, new W1NS3NT_BOT(token));
+			System.out.println("Success");
+			Thread.currentThread().join();
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		System.out.println("Failure");
 	}
 
 }
