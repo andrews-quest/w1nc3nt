48217c8b190d2d63befba4e290154ab3f1903ae3
andrews-quest <true.andrewsquest@gmail.com>
2024-04-17 20:29:04 +0200

    The Application is now put on Spring rails.

--- src/main/java/com/space_asians/w1nsent/W1nsentApplication.java
+++ src/main/java/com/space_asians/w1nsent/W1nsentApplication.java
@@ -1,25 +1,30 @@
 package com.space_asians.w1nsent;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 
-
-
+@Component("Application")
+@Primary
 public class W1nsentApplication {
 
-	public static void main(String[] args) {
-		// SpringApplication.run(W1nsentApplication.class, args);
+	@Value("${telegram.bot.test_token}")
+	String token;
 
-
-
-		String token = "";
-		try(TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
-			botsApplication.registerBot(token, new W1NS3NT_BOT(token));
+	@PostConstruct
+	public void run() {
+		try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+			botsApplication.registerBot(this.token, new W1NS3NT_BOT(this.token));
 			System.out.println("Success");
 			Thread.currentThread().join();
-		}catch (Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		System.out.println("Failure");
 	}
-
 }
