72853e32dbafbe6cc638f23c0503068a03bceb72
Andrii Roshkovan <andrii.roshkovan@gmail.com>
2024-04-19 17:18:49 +0200

    Connected to a DB. Added an entity Transaction.

---
+++ src/main/java/com/space_asians/w1ncent/entities/Transaction.java
@@ -0,0 +1,28 @@
+package com.space_asians.w1ncent.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "transactions")
+public class Transaction {
+
+    @Id
+    @Column
+    private int id;
+
+    @Column
+    private String when;
+    @Column
+    private String who;
+    @Column
+    private String whom;
+    @Column
+    private float how_much;
+    @Column
+    private String for_what;
+}
