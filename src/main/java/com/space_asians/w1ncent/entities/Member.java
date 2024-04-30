200484647f1c6939e90e694da2fd6489542bab9e
Andrii Roshkovan <andrii.roshkovan@gmail.com>
2024-04-30 19:06:31 +0200

    No lombok.

--- src/main/java/com/space_asians/w1ncent/entities/Member.java
+++ src/main/java/com/space_asians/w1ncent/entities/Member.java
@@ -1,10 +1,8 @@
 package com.space_asians.w1ncent.entities;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 @Entity
-@Data
 @Table(name = "members")
 public class Member {
     @Id
@@ -29,4 +27,115 @@ public class Member {
 
     @Column
     private Long chat_id;
+
+    public Member() {
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public float getBalance() {
+        return this.balance;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public String getState_manager() {
+        return this.state_manager;
+    }
+
+    public Long getChat_id() {
+        return this.chat_id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setState_manager(String state_manager) {
+        this.state_manager = state_manager;
+    }
+
+    public void setChat_id(Long chat_id) {
+        this.chat_id = chat_id;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Member)) return false;
+        final Member other = (Member) o;
+        if (!other.canEqual((Object) this)) return false;
+        if (this.getId() != other.getId()) return false;
+        final Object this$name = this.getName();
+        final Object other$name = other.getName();
+        if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
+        if (Float.compare(this.getBalance(), other.getBalance()) != 0) return false;
+        final Object this$password = this.getPassword();
+        final Object other$password = other.getPassword();
+        if (this$password == null ? other$password != null : !this$password.equals(other$password)) return false;
+        final Object this$state = this.getState();
+        final Object other$state = other.getState();
+        if (this$state == null ? other$state != null : !this$state.equals(other$state)) return false;
+        final Object this$state_manager = this.getState_manager();
+        final Object other$state_manager = other.getState_manager();
+        if (this$state_manager == null ? other$state_manager != null : !this$state_manager.equals(other$state_manager))
+            return false;
+        final Object this$chat_id = this.getChat_id();
+        final Object other$chat_id = other.getChat_id();
+        if (this$chat_id == null ? other$chat_id != null : !this$chat_id.equals(other$chat_id)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Member;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        result = result * PRIME + this.getId();
+        final Object $name = this.getName();
+        result = result * PRIME + ($name == null ? 43 : $name.hashCode());
+        result = result * PRIME + Float.floatToIntBits(this.getBalance());
+        final Object $password = this.getPassword();
+        result = result * PRIME + ($password == null ? 43 : $password.hashCode());
+        final Object $state = this.getState();
+        result = result * PRIME + ($state == null ? 43 : $state.hashCode());
+        final Object $state_manager = this.getState_manager();
+        result = result * PRIME + ($state_manager == null ? 43 : $state_manager.hashCode());
+        final Object $chat_id = this.getChat_id();
+        result = result * PRIME + ($chat_id == null ? 43 : $chat_id.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "Member(id=" + this.getId() + ", name=" + this.getName() + ", balance=" + this.getBalance() + ", password=" + this.getPassword() + ", state=" + this.getState() + ", state_manager=" + this.getState_manager() + ", chat_id=" + this.getChat_id() + ")";
+    }
 }
--- src/main/java/com/space_asians/w1ncent/entities/Transaction.java
+++ src/main/java/com/space_asians/w1ncent/entities/Transaction.java
@@ -1,12 +1,10 @@
 package com.space_asians.w1ncent.entities;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 import java.time.LocalDate;
 
 @Entity
-@Data
 @Table(name = "transactions")
 public class Transaction {
 
@@ -25,4 +23,101 @@ public class Transaction {
     private float how_much;
     @Column
     private String for_what;
+
+    public Transaction() {
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public LocalDate getWhen() {
+        return this.when;
+    }
+
+    public String getWho() {
+        return this.who;
+    }
+
+    public String getWhom() {
+        return this.whom;
+    }
+
+    public float getHow_much() {
+        return this.how_much;
+    }
+
+    public String getFor_what() {
+        return this.for_what;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setWhen(LocalDate when) {
+        this.when = when;
+    }
+
+    public void setWho(String who) {
+        this.who = who;
+    }
+
+    public void setWhom(String whom) {
+        this.whom = whom;
+    }
+
+    public void setHow_much(float how_much) {
+        this.how_much = how_much;
+    }
+
+    public void setFor_what(String for_what) {
+        this.for_what = for_what;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Transaction)) return false;
+        final Transaction other = (Transaction) o;
+        if (!other.canEqual((Object) this)) return false;
+        if (this.getId() != other.getId()) return false;
+        final Object this$when = this.getWhen();
+        final Object other$when = other.getWhen();
+        if (this$when == null ? other$when != null : !this$when.equals(other$when)) return false;
+        final Object this$who = this.getWho();
+        final Object other$who = other.getWho();
+        if (this$who == null ? other$who != null : !this$who.equals(other$who)) return false;
+        final Object this$whom = this.getWhom();
+        final Object other$whom = other.getWhom();
+        if (this$whom == null ? other$whom != null : !this$whom.equals(other$whom)) return false;
+        if (Float.compare(this.getHow_much(), other.getHow_much()) != 0) return false;
+        final Object this$for_what = this.getFor_what();
+        final Object other$for_what = other.getFor_what();
+        if (this$for_what == null ? other$for_what != null : !this$for_what.equals(other$for_what)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Transaction;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        result = result * PRIME + this.getId();
+        final Object $when = this.getWhen();
+        result = result * PRIME + ($when == null ? 43 : $when.hashCode());
+        final Object $who = this.getWho();
+        result = result * PRIME + ($who == null ? 43 : $who.hashCode());
+        final Object $whom = this.getWhom();
+        result = result * PRIME + ($whom == null ? 43 : $whom.hashCode());
+        result = result * PRIME + Float.floatToIntBits(this.getHow_much());
+        final Object $for_what = this.getFor_what();
+        result = result * PRIME + ($for_what == null ? 43 : $for_what.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "Transaction(id=" + this.getId() + ", when=" + this.getWhen() + ", who=" + this.getWho() + ", whom=" + this.getWhom() + ", how_much=" + this.getHow_much() + ", for_what=" + this.getFor_what() + ")";
+    }
 }
